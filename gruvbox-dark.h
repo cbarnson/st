2985ddf4e4e49ef81508f307891562045c4b54d3
Cody Barnson <cbarnson@outlook.com>
2020-10-05 21:34:13 -0600

    Monday_2020-10-05

--- gruvbox-dark.h
+++ gruvbox-dark.h
@@ -3,21 +3,9 @@
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-    "#cc241d",
-    "#98971a",
-    "#d79921",
-    "#458588",
-    "#b16286",
-    "#689d6a",
-    "#a89984",
-    "#928374",
-    "#fb4934",
-    "#b8bb26",
-    "#fabd2f",
-    "#83a598",
-    "#d3869b",
-    "#8ec07c",
-    "#ebdbb2",
+    "#cc241d", "#98971a", "#d79921", "#458588", "#b16286",
+    "#689d6a", "#a89984", "#928374", "#fb4934", "#b8bb26",
+    "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#ebdbb2",
 };
 
 /*
